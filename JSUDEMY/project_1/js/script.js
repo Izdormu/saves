6b6c2dc418e7e38986daebc5faad5cdd55f9d18e
Izdormu <49407959+Izdormu@users.noreply.github.com>
2021-06-09 12:58:28 +0200

    Add files via upload

---
+++ JSUDEMY/project_1/js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели","");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+
+for(let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотреных фильмов",""),
+          b = prompt("На сколько оцените его","");
+
+    personalMovieDB.movies[a] = b;
+}
+    
+console.log(personalMovieDB);
+
+
